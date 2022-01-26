b0f32f4d4e36c1ad1f9662b48cc09a914b5feb38
Chris Tieman <iam@christieman.com>
2022-01-26 17:01:49 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ THEME_PATH="$ZSH_CUSTOM/themes"
 
 echo "Linking theme file..."
 echo "$THEME_NAME -> $THEME_PATH/$THEME_NAME"
-ln "./$THEME_NAME" "$THEME_PATH/$THEME_NAME"
+ln "$(pwd)/$THEME_NAME" "$THEME_PATH/$THEME_NAME"
 
 echo -e "\nDone, load the theme via your zshrc:"
 echo "ZSH_THEME=\"duckster\""
